03824e30dad3a1d457d07521f9e9415e42e899d3
Erik Sorgaard <eriks@Eriks-MacBook-Pro.local>
2017-07-24 20:00:10 +0200

    saving

---
+++ xid_generator.go
@@ -0,0 +1,22 @@
+package xidgenerator
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+const (
+	X_ID_LENGHTH = 12
+)
+
+var (
+	xIdChars = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+)
+
+func generateXiD() string {
+	chars := make([]rune, X_ID_LENGHTH)
+	for char := range chars {
+		chars[char] = xIdChars[rand.Intn(len(xIdChars))]
+	}
+	return fmt.Sprintf("PUA-%s", string(chars))
+}
